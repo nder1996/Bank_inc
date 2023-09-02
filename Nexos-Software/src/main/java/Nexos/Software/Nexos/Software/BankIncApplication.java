fe53f5a0afc62e396cd19fd3a76203d9a5a468d9
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2023-09-01 22:13:07 -0500

    entorno de desarrollo

---
+++ Nexos-Software/src/main/java/Nexos/Software/Nexos/Software/BankIncApplication.java
@@ -0,0 +1,13 @@
+package Nexos.Software.Nexos.Software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankIncApplication.class, args);
+	}
+
+}
